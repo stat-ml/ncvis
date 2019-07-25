4d5f5ae8a10746b61df2f42ea1f9320bda241f8a
Aleksandr Artemenkov <alartum@gmail.com>
2019-07-26 00:52:59 +0300

    Changed plot color scheme

--- examples/utils/__init__.py
+++ examples/utils/__init__.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from cycler import cycler
 
-def scatter_classes(xs, y, ax, marker='o', silhouette_coefficient=False):
+from scipy.interpolate import interp1d
+def scatter_classes(xs, y, ax, show_labels=True, silhouette_coefficient=False, **kwargs):
     if silhouette_coefficient:
         from sklearn.metrics import silhouette_samples
         scores = silhouette_samples(xs, y)
@@ -16,6 +18,14 @@ def scatter_classes(xs, y, ax, marker='o', silhouette_coefficient=False):
         props = dict(boxstyle='round', facecolor='grey', alpha=0.1, linewidth=0)
         info = 'Silhouette Coefficient = {:.3f}±{:.3f}'
     
+    # Interpolating the discrete colormap
+    n = 12
+    ps = np.linspace(0, 1, n)
+    cs = plt.cm.Set3(ps)
+    f = interp1d(ps, cs.T)
+    nclasses = len(np.unique(y))
+    custom_cycler = cycler(color=f(np.linspace(0, 1, nclasses)).T)
+    ax.set_prop_cycle(custom_cycler)
     n_dims = xs.shape[-1]
     for k in np.unique(y):
         npoints = np.count_nonzero(y==k)
@@ -28,9 +38,9 @@ def scatter_classes(xs, y, ax, marker='o', silhouette_coefficient=False):
         elif n_dims == 1:
             x1 = xs[class_mask, 0]
             x2 = np.random.uniform(0, 1, npoints)
-        ax.scatter(x1, x2, marker=marker, label="{}".format(k))
+        ax.scatter(x1, x2, label="{}".format(k), **kwargs)
         
-    if len(np.unique(y)) <= 10:
+    if show_labels:
         ax.legend(loc='upper right')
     if silhouette_coefficient:
         ax.text(0.01, 0.01, info.format(score, std_score), fontsize=14, bbox=props, transform=ax.transAxes)
