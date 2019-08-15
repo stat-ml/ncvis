666a51cb610db84bb8dcba659a2a5e059ad0b43c
Aleksandr Artemenkov <alartum@gmail.com>
2019-08-16 00:11:48 +0300

    Correct os name

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -4,11 +4,11 @@ else
     conda_name="Linux"
 fi
 
-curl https://repo.continuum.io/miniconda/Miniconda3-latest-$(conda_name)-x86_64.sh -o ~/miniconda.sh
+curl "https://repo.continuum.io/miniconda/Miniconda3-latest-$conda_name-x86_64.sh" -o ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 
 conda update --yes conda
 conda create --yes -n test python=$TRAVIS_PYTHON_VERSION
 source activate test
-conda install conda-build
+conda install --yes conda-build
