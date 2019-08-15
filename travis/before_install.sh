b6172c16849175cc8d4b93a4cd1565dbecc186af
Aleksandr Artemenkov <alartum@gmail.com>
2019-08-16 00:03:20 +0300

    Wget to curl

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,16 +1,14 @@
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
-    brew update
-    brew install wget
     conda_name="MacOSX"
 else
     conda_name="Linux"
 fi
 
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+curl https://repo.continuum.io/miniconda/Miniconda3-latest-$(conda_name)-x86_64.sh -o ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 
 conda update --yes conda
 conda create --yes -n test python=$TRAVIS_PYTHON_VERSION
-conda activate test
+source activate test
 conda install conda-build
