018fabe183d264b4ad7f758a329d218381b70fb0
Aleksandr Artemenkov <alartum@gmail.com>
2019-08-19 15:38:57 +0300

    Conda upload

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -8,8 +8,10 @@ curl "https://repo.continuum.io/miniconda/Miniconda3-latest-$conda_name-x86_64.s
 bash ~/miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 
-conda update --yes conda
-conda create --yes -n test python=$TRAVIS_PYTHON_VERSION
+conda config --set always_yes yes
+conda config --set conda_build.anaconda_token $CONDA_UPLOAD_TOKEN
+conda update -q conda
+conda create -n test python=$TRAVIS_PYTHON_VERSION
 #Using source as shell has not been properly configured to use 'conda activate'.
 source activate test
-conda install --yes conda-build conda-verify
+conda install conda-build conda-verify anaconda-client
--- travis/install.sh
+++ travis/install.sh
@@ -3,4 +3,4 @@ export PATH="$HOME/miniconda/bin:$PATH"
 source activate test
 
 conda info
-conda build . 
+conda build .
