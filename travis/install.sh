293facb555b9f399bcca3f0d2c01d282b6ae4f2c
Aleksandr Artemenkov <alartum@gmail.com>
2019-08-25 20:32:02 +0300

    Install fixes

--- travis/install.sh
+++ travis/install.sh
@@ -6,6 +6,11 @@ conda info
 conda build .
 #Push to PyPI only once on master
 if [ $TRAVIS_BRANCH = 'master' ] && [ $TRAVIS_EVENT_TYPE = 'push' ] && [ $TRAVIS_OS_NAME = 'linux' ]; then
+    #Install conda dependencies
+    conda install numpy cython twine
+    #Install C++ libraries
+    git clone https://github.com/nmslib/hnswlib.git $(LibDir)hnswlib
+	git clone https://github.com/imneme/pcg-cpp $(LibDir)pcg-cpp 
     python setup.py sdist
     twine upload -u '__token__' -p $PYPI_UPLOAD_TOKEN dist/*
 fi
