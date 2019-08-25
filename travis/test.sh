9e7994173130884f424f5979e9126600f29ee8ec
Aleksandr Artemenkov <alartum@gmail.com>
2019-08-25 21:19:44 +0300

    Moved test to separate file

--- travis/test.sh
+++ travis/test.sh
@@ -2,6 +2,5 @@ export PATH="$HOME/miniconda/bin:$PATH"
 #Using source as shell has not been properly configured to use 'conda activate'.
 source activate test
 
-conda install conda-build conda-verify
-conda info
-conda build -b .
+CONDA_PKG=$(conda build . --output)
+conda build -t $CONDA_PKG
