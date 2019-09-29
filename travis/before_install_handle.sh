028691736a0bc91751012b69faf198684862f2f7
Aleksandr Artemenkov <alartum@gmail.com>
2019-09-29 23:33:06 +0300

    Fixed yml

--- travis/before_install_handle.sh
+++ travis/before_install_handle.sh
@@ -1,17 +1,16 @@
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
     conda_name="MacOSX"
     conda_ext="sh"
+    script_ext="sh"
 elif [ $TRAVIS_OS_NAME = 'linux' ]; then
     conda_name="Linux"
     conda_ext="sh"
+    script_ext="sh"
 elif [ $TRAVIS_OS_NAME = 'windows' ]; then
     conda_name="Windows"
     conda_ext="exe"
+    script_ext="cmd"
 fi
 
 curl "https://repo.anaconda.com/miniconda/Miniconda3-latest-$conda_name-x86_64.$conda_ext" -o ~/miniconda.$conda_ext
-if [ $TRAVIS_OS_NAME = 'windows' ]; then
-    travis/before_install.cmd
-else
-    travis/before_install.sh
-fi
+travis/before_install.$script_ext
