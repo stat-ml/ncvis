c87f23f3e9241b673b6268649e48f49343b0247f
Aleksandr Artemenkov <alartum@gmail.com>
2019-09-29 23:19:06 +0300

    cmd to call

--- travis/prepare.cmd
+++ travis/prepare.cmd
@@ -1,2 +1,6 @@
 %UserProfile%\miniconda.exe /InstallationType=JustMe /RegisterPython=0 /S /D=%UserProfile%\miniconda
-cmd.exe /K %UserProfile%\miniconda\Scripts\activate.bat %UserProfile%\miniconda
+call %UserProfile%\miniconda\Scripts\activate.bat %UserProfile%\miniconda
+conda config --set always_yes yes --set anaconda_upload no
+
+conda update -q conda
+conda create -n test python=$TRAVIS_PYTHON_VERSION
