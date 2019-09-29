d78524809e0ecbdffd6a3719df28e04ca5db8c63
Aleksandr Artemenkov <alartum@gmail.com>
2019-09-29 17:12:46 +0300

    Initial windows on travis

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,12 +1,20 @@
 if [ $TRAVIS_OS_NAME = 'osx' ]; then
     conda_name="MacOSX"
-else
+elif [ $TRAVIS_OS_NAME = 'linux' ]; then
     conda_name="Linux"
+else
+    conda_name="Windows"
+    choco install curl
 fi
 
-curl "https://repo.continuum.io/miniconda/Miniconda3-latest-$conda_name-x86_64.sh" -o ~/miniconda.sh
-bash ~/miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
+if [ $TRAVIS_OS_NAME != 'windows' ]; then
+    curl "https://repo.continuum.io/miniconda/Miniconda3-latest-$conda_name-x86_64.sh" -o ~/miniconda.sh
+    bash ~/miniconda.sh -b -p $HOME/miniconda
+    export PATH="$HOME/miniconda/bin:$PATH"
+else
+    curl "https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
+    start /wait "" Miniconda4-latest-Windows-x86_64.exe /InstallationType=JustMe /RegisterPython=0 /S /D=%UserProfile%\Miniconda3
+fi
 
 conda config --set always_yes yes --set anaconda_upload no
 
