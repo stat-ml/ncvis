0b6b242599656e4907fad79b5adeaa68face6a3c
Aleksandr Artemenkov <alartum@gmail.com>
2019-10-01 01:42:55 +0300

    Removed bad test

--- recipe/test.py
+++ recipe/test.py
@@ -8,7 +8,6 @@ def test_distances():
         vis = ncvis.NCVis(n_neighbors=15, M=16, ef_construction=200,
         n_init_epochs=20, n_epochs=50, min_dist=0.4, n_threads=-1, distance=distance)
         Y = vis.fit_transform(X)
-        Y[0, 0] = np.inf
         all_finite = np.all(np.isfinite(Y)) 
         print("Distance:", distance)
         print("Input:")
