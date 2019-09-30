c51e97bdaec671dcee2f30de7fc7a8833789340a
Aleksandr Artemenkov <alartum@gmail.com>
2019-10-01 02:32:21 +0300

    Testing without pytest

--- recipe/test.py
+++ recipe/test.py
@@ -1,20 +1,17 @@
 import numpy as np
 import ncvis
 
-def test_init():
-  vis = ncvis.NCVis(n_neighbors=15, M=16, ef_construction=200, n_init_epochs=20, n_epochs=50, min_dist=0.4, n_threads=-1, distance='euclidean')
-
 X = np.random.random((5, 3))
 distances=['euclidean', 'cosine', 'correlation', 'inner_product']
-def test_distances():
-    for distance in distances:
-        vis = ncvis.NCVis(n_neighbors=15, M=16, ef_construction=200,
-        n_init_epochs=20, n_epochs=50, min_dist=0.4, n_threads=-1, distance=distance)
-        Y = vis.fit_transform(X)
-        all_finite = np.all(np.isfinite(Y)) 
+for distance in distances:
+    vis = ncvis.NCVis(n_neighbors=15, M=16, ef_construction=200,
+    n_init_epochs=20, n_epochs=50, min_dist=0.4, n_threads=-1, distance=distance)
+    Y = vis.fit_transform(X)
+    all_finite = np.all(np.isfinite(Y))
+    if not all_finite: 
         print("Distance:", distance)
         print("Input:")
         print(X)
         print("Output:")
         print(Y)
-        assert all_finite
+        exit(1)
