1786ce1f70344ca0e44ffc8fc2741fbf46c9fb3f
Aleksandr Artemenkov <alartum@gmail.com>
2019-10-01 02:06:31 +0300

    Focused on osx

--- recipe/test.py
+++ recipe/test.py
@@ -1,6 +1,9 @@
 import numpy as np
 import ncvis
 
+def test_init():
+  vis = ncvis.NCVis(n_neighbors=15, M=16, ef_construction=200, n_init_epochs=20, n_epochs=50, min_dist=0.4, n_threads=-1, distance='euclidean')
+
 X = np.random.random((5, 3))
 distances=['euclidean', 'cosine', 'correlation', 'inner_product']
 def test_distances():
