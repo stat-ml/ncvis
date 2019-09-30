5703fe608c08cda20d8e44af325198e0ebb6e065
Aleksandr Artemenkov <alartum@gmail.com>
2019-09-30 11:46:16 +0300

    Windows upload

--- travis/upload.cmd
+++ travis/upload.cmd
@@ -3,7 +3,6 @@ call %UserProfile%\miniconda\Scripts\activate.bat %UserProfile%\miniconda
 call conda activate test
 call conda install anaconda-client
 
-call conda build . --output
+for /f %%i in ("conda build . --output") do set CONDA_PKG=%%i
 REM Upload to Anaconda Cloud
-REM CONDA_PKG=$(conda build . --output)
-REM anaconda -t $CONDA_UPLOAD_TOKEN upload $CONDA_PKG --force
+call anaconda -t %CONDA_UPLOAD_TOKEN% upload %CONDA_PKG% --force
