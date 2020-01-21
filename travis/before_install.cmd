7400427b617a72f4156953fc0e4a91a20b8ccac1
Aleksandr Artemenkov <alartum@gmail.com>
2020-01-22 01:04:35 +0300

    Testing Azure Pipelines

--- travis/before_install.cmd
+++
@@ -1,5 +0,0 @@
-%UserProfile%\miniconda.exe /InstallationType=JustMe /RegisterPython=0 /S /D=%UserProfile%\miniconda
-call %UserProfile%\miniconda\Scripts\activate.bat %UserProfile%\miniconda
-call conda config --set always_yes yes --set anaconda_upload no
-call conda update -q conda
-call conda create -n test python=%CONDA_PYTHON_VERSION%
--- travis/build.cmd
+++
@@ -1,4 +0,0 @@
-call %UserProfile%\miniconda\Scripts\activate.bat %UserProfile%\miniconda
-call conda activate test
-
-call conda build .
